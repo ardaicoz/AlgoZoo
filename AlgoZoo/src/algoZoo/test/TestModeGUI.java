bde975019e6028ef88f1f6498c9731da21ab7792
Arda İçöz <arda.icoz@ug.bilkent.edu.tr>
2020-05-23 03:52:33 +0300

    Ressetling Javadocs in Test mode.

--- AlgoZoo/src/algoZoo/test/TestModeGUI.java
+++ AlgoZoo/src/algoZoo/test/TestModeGUI.java
@@ -14,8 +14,9 @@ import javax.sound.sampled.Clip;
 import javax.swing.*;
 
 /**
- *
+ * The class which represents the graphical user interface with mapView and three options
  * @author Kerem, Arda
+ * @version 1.0
  */
 public class TestModeGUI extends javax.swing.JPanel {
 
@@ -171,7 +172,6 @@ public class TestModeGUI extends javax.swing.JPanel {
 
    /**
     * Get method to get the model of Test mode.
-    *
     * @return the model as TestModeModel class
     */
    public TestModeModel getModel() {
@@ -192,7 +192,6 @@ public class TestModeGUI extends javax.swing.JPanel {
 
    /**
     * Determines which level will start.
-    *
     * @param level level number.
     */
    public void setCurrentLevel(int level) {
@@ -219,7 +218,6 @@ public class TestModeGUI extends javax.swing.JPanel {
    
    /**
     * Sets the animal to be played with
-    *
     * @param animal animal chosen to be played with
     */
    public void setAnimal(Animal animal) {
@@ -228,13 +226,16 @@ public class TestModeGUI extends javax.swing.JPanel {
    
    /**
     * Gets the current level of the test mode
-    *
     * @return the processing level of the test mode
     */
    public int getLevel(){
       return currentLevel.getLevel();
    }
    
+   /**
+    * A method to indicate if the bee is moving or not.
+    * @return a boolean expression about bee's movement
+    */
    public boolean isBeeMoving() {
        return map.isBeeMoving();
    }
@@ -271,7 +272,10 @@ public class TestModeGUI extends javax.swing.JPanel {
       add(background);
       background.setBounds(0, 0, 1400, 800);
    }// </editor-fold>//GEN-END:initComponents
-
+   /**
+    * This method indicates the statements when retry button is pressed.
+    * @param evt 
+    */
     private void retryButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_retryButtonActionPerformed
       if(isBeeMoving()) { }
       else {
